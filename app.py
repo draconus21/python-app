37446310adf3c1f48ed2e7fdbd6af1eb1f6e526b
Neeth Kunnath <neeth.xavier@gmail.com>
2024-07-30 14:31:49 -0400

    part 1

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080)
+    app.run(host="127.0.0.1", port=8080, debug=True)
--- people.py
+++ people.py
@@ -1,5 +1,5 @@
 # people.py
-
+from flask import abort, make_response
 from datetime import datetime
 
 
@@ -28,3 +28,41 @@ PEOPLE = {
 
 def read_all():
     return list(PEOPLE.values())
+
+
+def read_one(lname):
+    if lname in PEOPLE:
+        return PEOPLE[lname]
+
+    abort(406, f"{lname} not found")
+
+
+def update(lname, person):
+    if lname not in PEOPLE:
+        abort(404, f"{lname} not found")
+
+    PEOPLE[lname]["fname"] = person.get("fname", PEOPLE[lname]["fname"])
+    PEOPLE[lname]["timestamp"] = get_timestamp()
+    return PEOPLE[lname]
+
+
+def delete(lname):
+    if lname not in PEOPLE:
+        abort(404, f"{lname} not found")
+
+    del PEOPLE[lname]
+    return make_response(f"{lname} successfully deleted", 200)
+
+
+def create(person):
+    lname = person.get("lname")
+    fname = person.get("lname", "")
+    if lname and lname not in PEOPLE:
+        PEOPLE[lname] = {
+            "lname": lname,
+            "fname": fname,
+            "timestamp": get_timestamp(),
+        }
+        return PEOPLE[lname], 201
+
+    abort(406, f"{lname} already exists")
